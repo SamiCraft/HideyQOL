855f445e801eaac456cb24e6449da88b51a89e04
rooot <hey@rooot.gay>
2023-09-13 19:53:33 +0200

    feat: massively simplify
    Signed-off-by: rooot <hey@rooot.gay>

--- src/main/java/com/samifying/hideyqol/listeners/MendingItemSaveListener.java
+++ src/main/java/com/samifying/hideyqol/listeners/MendingItemSaveListener.java
@@ -1,86 +1,46 @@
 package com.samifying.hideyqol.listeners;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerItemDamageEvent;
-import org.bukkit.event.player.PlayerItemMendEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataType;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MendingItemSaveListener implements Listener {
-    private static final NamespacedKey HIDEYQOL_UNUSABLE = new NamespacedKey("hideyqol", "unusable");
-    private static final Component UNUSABLE_LORE = Component.text("Broken").color(TextColor.color(0xFF0000));
-
     @EventHandler
     public void onItemDamage(PlayerItemDamageEvent event) {
         ItemStack item = event.getItem();
-        Material type = item.getType();
 
         if (isUnusable(item)) {
             event.setCancelled(true);
             return;
         }
 
-        int maxDurability = type.getMaxDurability();
+        int maxDurability = item.getType().getMaxDurability();
         int totalDamage = ((Damageable) item.getItemMeta()).getDamage() + event.getDamage();
         int targetDurability = maxDurability - totalDamage;
 
         if (targetDurability <= 1) {
             ItemMeta meta = item.getItemMeta();
-            meta.getPersistentDataContainer().set(HIDEYQOL_UNUSABLE, PersistentDataType.BOOLEAN, true);
             ((Damageable) meta).setDamage(maxDurability - 1);
             item.setItemMeta(meta);
-
-            List<Component> lore = item.lore();
-            if (lore == null) {
-                lore = new ArrayList<>();
-            }
-            lore.add(UNUSABLE_LORE);
-            item.lore(lore);
 
             event.setCancelled(true);
         }
     }
-    
-    @EventHandler
-    public void onItemRepair(PlayerItemMendEvent event) {
-        System.out.println("item repaired !");
-        ItemStack item = event.getItem();
-        Material type = item.getType();
-
-        int maxDurability = type.getMaxDurability();
-        int totalDamage = ((Damageable) item.getItemMeta()).getDamage() - event.getRepairAmount();
-        int targetDurability = maxDurability - totalDamage;
-
-        if (targetDurability > 1) {
-            ItemMeta meta = item.getItemMeta();
-
-            meta.getPersistentDataContainer().remove(HIDEYQOL_UNUSABLE);
-            ((Damageable) meta).setDamage(totalDamage);
-            item.setItemMeta(meta);
-
-            List<Component> lore = item.lore();
-            if (lore != null) {
-                lore.remove(UNUSABLE_LORE);
-                item.lore(lore);
-            }
-        }
-    }
 
     private boolean isUnusable(ItemStack item) {
-        return item.getItemMeta().getPersistentDataContainer().has(HIDEYQOL_UNUSABLE, PersistentDataType.BOOLEAN);
+        ItemMeta meta = item.getItemMeta();
+        if (!(meta instanceof Damageable)) return false;
+        if (!meta.hasEnchant(Enchantment.MENDING)) return false;
+
+        int durability = item.getType().getMaxDurability() - ((Damageable) meta).getDamage();
+        return durability <= 1;
     }
 
     // --- listeners for item usage ---
