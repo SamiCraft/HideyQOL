e29ed039ae5d0abc79c8ddc193d3255029d75d64
rooot <hey@rooot.gay>
2023-09-16 19:28:37 +0200

    feat: make mending tools unbreakable still work in progress

---
+++ src/main/java/com/samifying/hideyqol/listeners/MendingItemSaveListener.java
@@ -0,0 +1,75 @@
+package com.samifying.hideyqol.listeners;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemDamageEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class MendingItemSaveListener implements Listener {
+    @EventHandler
+    public void onItemDamage(PlayerItemDamageEvent event) {
+        ItemStack item = event.getItem();
+
+        if (isUnusable(item)) {
+            event.setCancelled(true);
+            return;
+        }
+
+        ItemMeta meta = item.getItemMeta();
+        if (!meta.hasEnchant(Enchantment.MENDING)) return;
+
+        int maxDurability = item.getType().getMaxDurability();
+        int totalDamage = ((Damageable) meta).getDamage() + event.getDamage();
+        int targetDurability = maxDurability - totalDamage;
+
+        if (targetDurability <= 1) {
+            ((Damageable) meta).setDamage(maxDurability - 1);
+            item.setItemMeta(meta);
+
+            event.setCancelled(true);
+        }
+    }
+
+    private boolean isUnusable(ItemStack item) {
+        ItemMeta meta = item.getItemMeta();
+        if (!(meta instanceof Damageable)) return false;
+        if (!meta.hasEnchant(Enchantment.MENDING)) return false;
+
+        int durability = item.getType().getMaxDurability() - ((Damageable) meta).getDamage();
+        return durability <= 1;
+    }
+
+    // --- listeners for item usage ---
+
+    @EventHandler
+    public void onPlayerBreakBlockEvent(BlockBreakEvent event) {
+        if (isUnusable(event.getPlayer().getInventory().getItemInMainHand())) {
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerDamageEntityEvent(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player player) {
+            if (isUnusable(player.getInventory().getItemInMainHand())) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInteract(PlayerInteractEvent event) {
+        if (isUnusable(event.getPlayer().getInventory().getItemInMainHand())) {
+            event.setCancelled(true);
+        }
+    }
+
+    // (are there more events that need to be cancelled?)
+}
